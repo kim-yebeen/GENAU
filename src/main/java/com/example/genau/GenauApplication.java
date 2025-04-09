c723cb2a2023cb98e28aa77a44ecf009f943c442
kim-yebeen <kimyebeen125@gmail.com>
2025-04-09 13:31:54 +0900

    GENAU 프로젝트 초기 세팅

---
+++ src/main/java/com/example/genau/GenauApplication.java
@@ -0,0 +1,13 @@
+package com.example.genau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/genau/GenauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
