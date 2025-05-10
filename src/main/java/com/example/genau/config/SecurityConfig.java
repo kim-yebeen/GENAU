4d502c58a1f0517c27f39f9ab2cef016b0e95aad
jiyun0852 <142162235+jiyun0852@users.noreply.github.com>
2025-05-11 01:58:23 +0900

    팀 스페이스 컨피그 수정

--- src/main/java/com/example/genau/config/SecurityConfig.java
+++ src/main/java/com/example/genau/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173")); // ✅ 프론트 주소
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Content-Disposition"));
         config.setAllowCredentials(true);
