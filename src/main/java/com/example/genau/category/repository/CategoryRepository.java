bbaa3b1c006b23efbfcc53cb0bb1d4a6b389abfe
jiyun0852 <142162235+jiyun0852@users.noreply.github.com>
2025-06-01 00:03:35 +0900

    목록 조회관련 수정

--- src/main/java/com/example/genau/category/repository/CategoryRepository.java
+++ src/main/java/com/example/genau/category/repository/CategoryRepository.java
@@ -4,6 +4,11 @@ import com.example.genau.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    // ✅ teamId로 모든 카테고리 조회
+    List<Category> findByTeam_TeamId(Long teamId);
 }
